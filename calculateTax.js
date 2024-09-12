8f384bbf2c88b795dbc1c4b88b7f1f64e737eb03
Anowar Manik <dev.anowarhmanik@gmail.com>
2024-09-12 18:42:20 +0600

    create Assignment-4 folder

---
+++ calculateTax.js
@@ -0,0 +1,14 @@
+function calculateTax(income, expenses) {
+    if(expenses>income ||income<0 ||expenses<0){
+        return "invalid input";
+       
+    }
+    let profit = income-expenses;
+       const tax =profit*20/100
+       return tax;
+    
+ 
+}
+     
+console.log(calculateTax(6000,-1500))
+
